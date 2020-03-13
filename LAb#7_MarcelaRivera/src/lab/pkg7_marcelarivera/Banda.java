113d50b1134cf0c62625710b3fa60dae5236ade8
Marcela <mvrivera@unitec.edu>
2020-03-13 14:41:03 -0600

    Das

---
+++ LAb#7_MarcelaRivera/src/lab/pkg7_marcelarivera/Banda.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab.pkg7_marcelarivera;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Mvrivera
+ */
+public class Banda extends Usuario{
+    private int num_integrantes;
+    private String usuario,contra,nombre;
+    private String genero;
+    private ArrayList<Canciones> canciones=new ArrayList();
+}
